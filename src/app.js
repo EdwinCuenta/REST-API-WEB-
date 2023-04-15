a4842895f389a5b2642a756009647d1adcc06e18
EdwinCuenta <83187876+EdwinCuenta@users.noreply.github.com>
2023-04-15 10:28:05 -0400

    primer commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {pool} from './db.js'
+import { PORT } from "./config.js";
+
+
+const app = express()
+
+app.get('/', async (req, res) => {
+    const [rows] = await pool.query('select * from peliculas')
+    res.json(rows)
+})
+
+app.get('/ping', async (req, res) => {
+    const [result] = await pool.query('SELECT "hello world " as RESULT')
+    console.log(result)
+
+    res.send('Bienvenido al Servidor')
+})
+
+app.get('/create', async (req, res) => {
+    const result = await pool.query()
+
+    res,json(result)
+})
+
+app.listen(PORT)
+console.log('Server on port 3000', PORT)
